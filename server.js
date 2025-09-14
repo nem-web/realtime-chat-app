6abab4ac65039335ffd8d915bad6c49b553a40fe
nem-web <nem-web@example.com>
2025-09-15 02:28:53 +0530

    server.js

--- server.js
+++ server.js
@@ -201,7 +201,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
